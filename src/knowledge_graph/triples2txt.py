ebd6b92da0d07f24700a1574af9b32c45b7a4644
Yuhao Mao <maoyuhao92@gmail.com>
2020-09-30 03:00:35 -0400

    triples2txt

---
+++ src/knowledge_graph/triples2txt.py
@@ -0,0 +1,21 @@
+from py2neo import Graph, Node, Relationship,NodeMatcher,cypher
+
+def find_kg_triples(username,password):
+    graph = Graph('http://localhost:7474', username= username, password=password)
+    string_qurey = "match (a)-[r]->(b) where type(r) in ['writers','stars','directors','genre'] return a.name,type(r),b.name;"
+    query_result = graph.run(string_qurey)
+    for i in query_result:
+        result = ""
+        head = i[0]
+        relation = i[1]
+        tail = i[2]
+        result += head + "\t" + relation + "\t" + tail + "\n"
+        f = open("../../data/movie/kg.txt", "a")
+        f.write(result)
+        f.close()
+
+username = "neo4j"
+password = "0905"
+find_kg_triples("neo4j","0905")
+
+
